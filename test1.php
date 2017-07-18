b54b0ba09ab115c8d934e792d9b8967e01bcff50
EmeraldGenevieve <ayumalay27@gmail.com>
2017-07-18 14:55:34 +0630

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Resume</title>
+<link rel="stylesheet" type="text/css" href="resume.css">
+
+</head>
+<body class="style">
+<?php header('Location: /test1.html'); ?>
+<div class="head">
+<h1 class="text1">Ei Shwe Sin Phyo</h1>
+<p class="text1">KOLEJ KEDIAMAN TUN ABDUL RAZAK (PFI2) <br> UNIVERSITI MALAYSIA PERLIS (UniMAP) KAMPUS PAUH PUTRA 02600 ARAU, PERLIS <br>Telephone: +60169565521<br>
+Email: eishwesinphyo0@gmail.com</div>
+<div class="header"><img src="DSC_0008.jpg"></div>
+<hr>
+<p class="text"><big><ins> Personal Details</big></ins></p>
+Passport No.: MA506700 <br>
+D.O.B: <i>22</i> October <i>1995</i><br>
+Gender: Female<br>
+Nationality: Myanmar<br>
+Marital Status: Single<br>
+<p class="text"><big><ins>Education</big></ins></p>
+<p><b>School</b></p> Thanlyin Technological University (Myanmar) <i>(2012-2013)</i><br> Universiti Malaysia Perlis, Arau, Perlis (UniMAP) <i>(2014-2019)</i><br>
+<p> <b>Major</b></p> Bachelor of Engineering (Honours) (Information and technology)<br> Bachelor of Engineering (Honours) (Computer Network Engineering)<br>
+CGPA: 2.37 <br>
+<p class="text"><big><ins>Extra-Curricular Activities</ins></p></big>
+Certificate from <q>International English Course</q> in UniMAP<i> (2014)</i><br>
+Participated and performed in role play of <q>Religions for Peace Program</q> in Myanmar <i>(2013)</i><br>
+Rover Trainings (Sea) (I, II) <i>(2014)</i><br>
+Joined 150<i>km</i> walking Program Organized by UNIMAP <i>(2014)</i><br>
+Participated in <q>PROGRAM TEKNOLOGI KEJAYAAN DIRI with NLP</q>(Neuro Linguistic Programming) <i>(2015)</i><br>
+Attended <q>Programming Languages Talk</q> presented by Prof. Dr. R Badlishah Ahmed in Pauh Putra UniMAP <i>(2016)</i><br>
+Performed in <q>International Students’ Cultural Night</q> (Singing) <i>(2016 & 2017)</i><br>
+Performed in YPGO (Youngpreneurs Go) “Talent Time” (Singing) <i>(2017)</i><br>
+<p class="text"><big><ins>Language Proficiency</ins></p></big>
+-Excellent in both written and spoken Myanmar<br>
+-Good in four skills of English Language<br>
+-Basic Malay language, some of basic Arabic, speaking & listening skills in Korean
+Skills <br>
+<p class="text"><big><ins>Leadership, Teamwork & Management</ins></p></big>
+-Able to work in group and individual environment<br>
+-I’m good at singing. And I also love to compose songs in my free time<br>
+-I like to participate in volunteering programs<br>
+-I’m friendly with everyone and can tolerate the worst situation with people
+around me<br>
+-I’m a kindly person with a good heart even though I’m not perfect<br>
+-Eager to learn new things<br>
+-Quick learner<br>
+<p class="text"><ins><big>Logical skills</big></p></ins>
+Initiator and passionate about working
+<p class="text"><ins><big>Programs I’ve learnt</big></p></ins>
+-Microsoft Office Tools; Words, Power Point, Excel<br>
+-AutoCAD
+-Basic programming, Digital electronics(I)(II), Analog electronics(I)(II)<br>
+-Electric circuit, Electrical Engineering<br>
+-Algorithms and data structures, Advanced Computer Network<br>
+-Microprocessor Systems, Principles of Computer Architecture<br>
+-Engineering skills and etc..
+<p class="text"><big><ins>References</ins></p></big>
+<mark>DR. RUZELITA NGADIRAN</mark><br>
+<i>Industrial Training Coordinator</i><br>
+<i>School of Computer and Communication Engineering</i><br>
+<i>Universiti Malaysia Perlis (UniMAP)</i><br>
+<b>Email: ruzelita@unimap.edu.my</b><br>
+<a href="https://www.gmail.com"> Click here </a>
+</body>
+</html>
